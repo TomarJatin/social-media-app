96b31aa12c842524edc2486a5574a6d1df83616b
Jatin Tomar <jt24072002@gmail.com>
2022-10-12 21:45:14 +0530

    mongodb url added to enviornment

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
@@ -10,13 +11,13 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
+dotenv.config();
 
 // Express Middlewares
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://tomarjatin:Jatin123@cluster0.t2wrcyd.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECT)
     .then(() => app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
